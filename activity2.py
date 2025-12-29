6e052af88013cd4b0b921663bd28aa62cf1e8fb4
smunni46@gmail.com <smunni46@gmail.com>
2025-12-28 21:38:31 -0600

    hello

---
+++ activity2.py
@@ -0,0 +1,16 @@
+a=10
+b=12
+c=12
+
+print(a != b)
+print(b != c)
+a="python"
+b="coding"
+if a != b:
+    print("a nd b are different")
+
+a=int(input("enter a number"))
+if a % 2 != 0:
+    print("a is not an even number")
+else : 
+    print("a is an even number")
