16e19a681a39d18908d11d09f6317b521eb42595
smunni46@gmail.com <smunni46@gmail.com>
2025-12-28 21:24:03 -0600

    first commit

---
+++ activity1.py
@@ -0,0 +1,19 @@
+a=10
+b=12
+c=0
+
+if a and b and c :
+    print("all the numbers have boolean value as true")
+else:
+    print("at least one number has boolean value false")
+
+
+
+a=10
+b=-10
+c=0
+
+if a>0 or b>0:
+    print("either of the numbers are greater than 0")
+else:
+    print("no number is greater then 0")
